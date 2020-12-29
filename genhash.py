0ddc38fb34eff0f5237bf27d01c0dff7611eccba
zidelnet <atoyebiopeyemi42@gmail.com>
2020-12-29 16:39:18 +0100

    Update the file

--- genhash.py
+++ genhash.py
@@ -4,6 +4,19 @@ import argparse
 import sys 
 import hashlib
 
+
+# function to convert values to md5 hash 
+def convert_to_md5(x): 
+	hash_object = hashlib.md5(x.encode())
+	md5_hash = hash_object.hexdigest() 
+	return md5_hash
+
+# function to convert values to sha1 hash 
+def convert_to_sha1(x): 
+	hash_object = hashlib.sha1(x.encode())
+	sha1_hash = hash_object.hexdigest() 
+	return sha1_hash
+
 # code banner 
 def banner(): 
 	print("""
@@ -17,30 +30,30 @@ def banner():
 	print("[*] such as MD5, SHA1 etc and its corresponding value")
 	print("[*] Tool developed by @zidelnet")
 	print("------------------------------------------------------")
-	print()
 	
 banner()
+print("""
+Sample Example: Generate MD5 Hash wordlist 
+# python genhash.py --hash MD5 --start 1111 --end 9999
+""")
+
 # Define the program description 
-text = "A tool to generate any type of hashed wordlist for brute forcing password during a web application attack"
-# python genhash.py  --hash MD5 --start 1111 --end 9999
-# python genhash.py -h MD5 -s 1111 -e 99999
-# python genhash.py --list
+text = "Generate hashed wordlist for brute forcing password during a web application attack"
 
 # Initiate the parser with a description 
 parser = argparse.ArgumentParser(description=text)
 parser.add_argument("-V", "--version", help="show program version", action="store_true")
 parser.add_argument("-L", "--list", help="show available hash", action="store_true")
-parser.add_argument("-H", "--hash", help="set the hash type")
+parser.add_argument("--hash", help="set the hash type")
 parser.add_argument("--start", type=int, help="the start value of the generator")
 parser.add_argument("--end", type=int, help="the end value of the generator")
-#parser.add_argument("")
-#parser.parse_args()
 
 # Read arguments from the command line
 args = parser.parse_args()
 
 # available hash type 
-hash_list_available = ["MD5","SHA1"]
+upper_list = ["MD5","SHA1","SHA224","SHA256","SHA384","SHA512"]
+hash_list_available = [i.lower() for i in upper_list]+upper_list 
 
 # check if no argument is given 
 if len(sys.argv) <= 1: 
@@ -54,8 +67,7 @@ if args.version:
 # display the available hash type 
 if args.list:
 	print("[*] Available hash")
-	for i in hash_list_available:
-		#print(i)
+	for i in upper_list:
 		print(f"--hash {i}")
 
 # check for --hash 
@@ -72,41 +84,33 @@ if args.end:
 	END_ = args.end 
 
 
-# function to convert values to md5 hash 
-def convert_to_md5(x): 
-	hash_object = hashlib.md5(x.encode())
-	md5_hash = hash_object.hexdigest() 
-	return md5_hash
+try: 
+	# generate sequencial numbers 
+	number_list = list(range(START_, END_,1))
 
-# function to convert values to sha1 hash 
-def convert_to_sha1(x): 
-	hash_object = hashlib.sha1(x.encode())
-	sha1_hash = hash_object.hexdigest() 
-	return sha1_hash
+	if selected_hash == 'md5': 
+		# convert the generated sequencial numbers into md5 hash 
+		hash_list = [convert_to_md5(str(i)) for i in number_list]
 
-# generate sequencial numbers 
-number_list = list(range(START_, END_,1))
+	if selected_hash == 'sha1': 
+		# convert the generated sequencial numbers into sha1 hash 
+		hash_list = [convert_to_sha1(str(i)) for i in number_list]
 
-if selected_hash == 'md5': 
-	# convert the generated sequencial numbers into md5 hash 
-	hash_list = [convert_to_md5(str(i)) for i in number_list]
+	# convert both list into a dictionary 
+	full_list = {hash_list[i]:number_list[i] for i in range(len(number_list))}
 
-if selected_hash == 'sha1': 
-	# convert the generated sequencial numbers into sha1 hash 
-	hash_list = [convert_to_sha1(str(i)) for i in number_list]
+	print(f"[+] {selected_hash}-list.txt created containing only the {selected_hash} Hash")
+	print(f"[+] full-list.txt created containing both the {selected_hash} Hash the corresponding string value")
 
-# convert both list into a dictionary 
-full_list = {hash_list[i]:number_list[i] for i in range(len(number_list))}
+	# write the md5 hash list into a file 
+	with open(selected_hash+"-list.txt","w") as f: 
+		for i in hash_list: 
+			f.write(i+"\n")
 
-print(f"[+] {selected_hash}-list.txt created containing only the {selected_hash} Hash")
-print(f"[+] full-list.txt created containing both the {selected_hash} Hash the corresponding string value")
+	# write the full-list into a file 
+	with open("full-list.txt","w") as f: 
+		for i,j in full_list.items(): 
+			f.write("{} : {}\n".format(i,j))
 
-# write the md5 hash list into a file 
-with open(selected_hash+"-list.txt","w") as f: 
-	for i in hash_list: 
-		f.write(i+"\n")
-
-#write the full-list into a file 
-with open("full-list.txt","w") as f: 
-	for i,j in full_list.items(): 
-		f.write("{} : {}\n".format(i,j))
+except NameError: 
+	pass 
